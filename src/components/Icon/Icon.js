b0f4de1587bcb6ac84d331a2a50d056623e2c7b3
karol <karol.rudzinski1990@gmail.com>
2022-04-07 14:05:24 +0200

    global icon className added, missing icon

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,6 +3,7 @@ import Suggestions from "../../assets/icons/suggestions";
 import Empty from "../../assets/icons/empty";
 import Bulb from "../../assets/icons/bulb";
 import VoteArrow from "../../assets/icons/votearrow";
+import TextBubble from "../../assets/icons/textbubble";
 
 const Icon = ({ icon, className }) => {
   const icons = {
@@ -10,13 +11,14 @@ const Icon = ({ icon, className }) => {
     empty: Empty,
     bulb: Bulb,
     votearrow: VoteArrow,
+    textbubble: TextBubble,
   };
 
   const Icon = icons[icon];
   return (
     <>
       {icon && (
-        <div className={className}>
+        <div className={`icon ${className}`}>
           <Icon />
         </div>
       )}

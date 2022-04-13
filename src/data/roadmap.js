fd7983cfbd7eb666a429ad635710750bdfb3139e
karol <karol.rudzinski1990@gmail.com>
2022-04-13 18:18:20 +0200

    data update

---
+++ src/data/roadmap.js
@@ -0,0 +1,23 @@
+export const roadmap = [
+  {
+    label: "planned",
+    description: "Ideas prioritized for research",
+    id: 1,
+    count: 2,
+    color: "orange",
+  },
+  {
+    label: "in-progress",
+    description: "Currently being developed",
+    id: 2,
+    count: 2,
+    color: "purple",
+  },
+  {
+    label: "live",
+    description: "Released features",
+    id: 3,
+    count: 1,
+    color: "blue",
+  },
+];

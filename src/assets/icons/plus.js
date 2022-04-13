f3632124aed99a4eef535cf8578090270d2140a8
karol <karol.rudzinski1990@gmail.com>
2022-04-13 18:16:27 +0200

    additional icons

---
+++ src/assets/icons/plus.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="17"
+      height="16"
+      fill="none"
+      viewBox="0 0 17 16">
+      <path
+        fill="#fff"
+        d="M10.342 16v-5.834h5.687V5.864h-5.687V0H5.746v5.864H0v4.302h5.746V16h4.596z"></path>
+    </svg>
+  );
+}
+
+export default Icon;

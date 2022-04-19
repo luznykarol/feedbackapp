993eb2770dd536a8e0b7f2ad21d36f7b3b551915
karol <karol.rudzinski1990@gmail.com>
2022-04-19 13:57:55 +0200

    tick icon added

---
+++ src/assets/icons/tick.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="11"
+      fill="none"
+      viewBox="0 0 13 11">
+      <path stroke="#AD1FEA" strokeWidth="2" d="M1 5.233L4.522 9 12 1"></path>
+    </svg>
+  );
+}
+
+export default Icon;
